74512462baa30ab66567ad3bab6b0aad5446740e
Jessie Oliver Mendez Cruz <212194360+FelixHyena@users.noreply.github.com>
2026-03-09 10:01:30 +0000

    V1

---
+++ Car.java
@@ -0,0 +1,55 @@
+/* 
+ * Jessie Mendez Cruz
+ * 3-9-2026
+ * Car Data Analyzer
+ */
+
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
+/*need: 
+- private fields for all columns
+- constructor
+- getters
+- toString() Car_ID, Brand, Model, Year,
+ Fuel_Type, Color, Mileage_kmpl
+*/
+
+public class Car{
+    //String set up
+    private String ID;
+    private String brand;
+    private String model;
+    private String year;
+    private String fuel;
+    private String color;
+    private String milage;
+
+    //getters
+    public String getID{
+        return ID;
+
+    public String getBrand{
+        return brand;
+    }
+    public String getModel{
+        return model;
+    }
+    public String getYear{
+        return year;
+    }
+    public String getFuel{
+        return fuel;
+    }
+    public String getColor{
+        return color;
+    }
+    public String getMilage{
+        return mileage;
+    }
+
+    }
+
+}
+
---
+++ Main.java
@@ -0,0 +1,7 @@
+public static void main(String[] args){
+
+private String fileName = "Car_Data.csv";
+
+ArrayList[Car]
+
+}
