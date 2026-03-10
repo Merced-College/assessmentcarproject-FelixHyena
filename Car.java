676e4dec7d3850b0f972efe945b8ab58374b57a2
Jessie Oliver Mendez Cruz <212194360+FelixHyena@users.noreply.github.com>
2026-03-10 01:43:53 +0000

    V3

--- Car.java
+++ Car.java
@@ -1,68 +1,63 @@
-/* 
- * Jessie Mendez Cruz
- * 3-9-2026
- * Car Data Analyzer
- */
+//Jessie Mendez Cruz
+//3-9-2026
+//Car Data Assessment
 
-//class that sets up variables, the constructor,
-//the getters, and the toString()
-
+//Class
 public class Car {
-    //variables
-    private int carId;
-    private String brand;
-    private String model;
-    private int year;
-    private String fuelType;
-    private double mileage;
-
-//constructor
-public Car(int carID, String brand, String model, 
-    int year, String fuelType, String color, double mileage){
-
-    this.carID = carID;
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.fuelType = fuelType;
-    this.color = color;
-    this.mileage = mileage;
-    }
-
-    //getters
-    public int getID(){
-        return carID;
-    }
-
-        public int getBrand(){
-        return brand;
-    }
-
-        public int getModel(){
-        return model;
-    }
-
-        public int getYear(){
-        return year;
-    }
-
-        public int getFuel(){
-        return fuelType;
-    }
-
-        public int getColor(){
-        return color;
-    }
-
-        public int getMileage(){
-        return mileage;
-    }
-
-    //print Car details
-    public String toString(){
-        return carID + " " + brand + " " + model + " " + year +
-           " " + fuelType + " " + color + " " + mileage;
-    }
-
+	//Variables
+	private String carID;
+	private String brand;
+	private String model;
+	private int year;
+	private String fuelType;
+	private String color;
+	private double mileage;
+	
+	//object
+	public Car(String carID, String brand, String model, int year, String fuelType, String color, double mileage) {
+		
+		this.carID = carID;
+		this.brand = brand;
+		this.model = model;
+		this.year = year;
+		this.fuelType = fuelType;
+		this.color = color;
+		this.mileage = mileage;
+		
+	}
+	
+	//getters
+	public String getCarID(){
+		return carID;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public String getFuelType() {
+		return fuelType;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public double getMileage() {
+		return mileage;
+	}
+	
+	//Prints car details from csv using toString()
+	
+	public String toString() {
+		return carID + " " + brand + " " + model + " " + year + " " + fuelType + " " + color + " " + mileage;
+	}
 }
-
--- Main.java
+++ Main.java
@@ -1,8 +1,66 @@
+//Jessie Mendez Cruz
+//3-9-2026
+//Car Data Assessment
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static ArrayList<Car> loadCars(String filename){
+    static ArrayList<Car> cars = new ArrayList<>();
 
+    public static void main(String[] args) {
+
+        // load cars from CSV file
+        cars = loadCars("Car_Data.csv");
+
+        // print first 10 cars to confirm loading worked
+        System.out.println("First 10 cars loaded:");
+
+        for (int i = 0; i < Math.min(10, cars.size()); i++) {
+            System.out.println(cars.get(i));
+        }
+    }
+
+    // load cars method that looks at and reads that file
+    public static ArrayList<Car> loadCars(String fileName) {
+
+        // creates new ArrayList that stores each object (car) from the file
+        ArrayList<Car> list = new ArrayList<>();
+        
+        //verifies that the file is read
+        try {  	
+            Scanner file = new Scanner(new File(fileName));
+            file.nextLine(); // skip header
+            
+            //parses and organizes the lines
+            while (file.hasNextLine()) {
+
+                String line = file.nextLine();
+                String[] parts = line.split(",");
+
+                String id = parts[0].trim();
+                String brand = parts[1];
+                String model = parts[2];
+                int year = Integer.parseInt(parts[3]);
+                String fuel = parts[4];
+                String color = parts[5];
+                double mileage = Double.parseDouble(parts[6]);
+
+                Car car = new Car(id, brand, model, year, fuel, color, mileage);
+                list.add(car);
+            }
+            // closes file
+            file.close();
+            
+            //in case file cant be read
+        } catch (Exception e) {
+            System.out.println("Error reading file.");
+        }
+        
+        //output of car list size
+        System.out.println("Total cars loaded: " + list.size());
+
+        return list;
     }
 }
+
